98f07d6638200f1af26bd3c1855e035112ea6a50
gacharya2022 <124161352+gacharya2022@users.noreply.github.com>
2023-02-13 20:42:54 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,21 @@
+let celcius=document.getElementById('celcius');
+let fahrenheit=document.getElementById('fahrenheit');
+let kelvin=document.getElementById('kelvin');
+celcius.oninput=function(){
+    let f=(parseFloat(celcius.value)*9)/5+32;
+    fahrenheit.value=parseFloat(f.toFixed(2));
+    let k=(parseFloat(fahrenheit.value)+273.15);
+    kelvin.value=parseFloat(k.toFixed(2));
+}
+fahrenheit.oninput=function(){
+    let c=((parseFloat(fahrenheit.value)-32)*5)/9;
+    celcius.value=parseFloat(c.toFixed(2));
+    let k=((parseFloat(fahrenheit.value)-32)*5)/9+273.15;
+    kelvin.value=parseFloat(k.toFixed(2));
+}
+kelvin.oninput=function(){
+    let c=(parseFloat(kelvin.value)+273.15);
+    celcius.value=parseFloat(c.toFixed(2));
+    let f=((parseFloat(kelvin.value)-273.15)*9)/5+32;
+    fahrenheit.value=parseFloat(f.toFixed(2));
+}
